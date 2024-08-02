8c83ef7b0cd98e0ef4225e27f0d859148c39561c
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-01 19:29:08 -0600

    first commit

---
+++ yolo_trainer.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+import torch
+from roboflow import Roboflow
+
+
+if __name__ == '__main__':
+
+    # device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    # print("Using:", device)
+    # torch.cuda.set_device(device)
+
+    from roboflow import Roboflow
+    rf = Roboflow(api_key="BS8zZk5oG44IuXWegBlr")
+    project = rf.workspace("agrobotics").project("yolo_new-4z2qz")
+    version = project.version(1)
+    dataset = version.download("yolov8")
+
+    model = YOLO("yolov8n.pt")
+    # model.class_weights = [3, 3, 1] # adjust weights to emphasize classes with less representation
+    model.train(data='yolo_new-1/data.yaml', epochs=300, patience=50)
